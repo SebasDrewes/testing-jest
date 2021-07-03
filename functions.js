98c22488b8630125faae1fe043edf73e90376674
Sebastian Drewes <sebas.drewes@gmail.com>
2021-07-02 22:27:05 -0300

    testado ceasarCipher todo menos Case

--- functions.js
+++ functions.js
@@ -14,8 +14,32 @@ calculator.suma = (a, b) => a + b;
 calculator.resta = (a, b) => a - b;
 calculator.division = (a, b) => a / b;
 calculator.multiplicacion = (a, b) => a * b;
+
+function caesarCipher(string, key) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let newString = '';
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] === '.') {
+      newString += '.';
+    }
+    for (let j = 0; j < alphabet.length; j += 1) {
+      if (string[i] === alphabet[j]) {
+        if (alphabet[j + key] === undefined) {
+          const negativeIndex = (j + key) % alphabet.length;
+          newString += alphabet[negativeIndex];
+          break;
+        } else {
+          newString += alphabet[j + key];
+        }
+      }
+    }
+  }
+  return newString;
+}
+
 module.exports = {
   capitalize,
   reverseString,
   calculator,
+  caesarCipher,
 };
--- functions.test.js
+++ functions.test.js
@@ -4,6 +4,7 @@ const myModule = require('./functions');
 const capitalize = myModule.capitalize;
 const reverseString = myModule.reverseString;
 const calculator = myModule.calculator;
+const caesarCipher = myModule.caesarCipher;
 
 test('Pass one', () => {
   expect(capitalize('sebastian')).toBe('Sebastian');
@@ -15,3 +16,13 @@ test('calculatorsum', () => { expect(calculator.suma(3, 3)).toBe(6); });
 test('calculatorrest', () => { expect(calculator.resta(3, 3)).toBe(0); });
 test('calculatordivision', () => { expect(calculator.division(3, 3)).toBe(1); });
 test('calculatormultiplicacion', () => { expect(calculator.multiplicacion(3, 3)).toBe(9); });
+
+test('ceasarCipherNormal', () => {
+  expect(caesarCipher('hello', 1)).toBe('ifmmp');
+});
+test('ceasarCipherPuntoYZ', () => {
+  expect(caesarCipher('z.o.o', 5)).toBe('e.t.t');
+});
+test('ceasarCipherCase', () => {
+  expect(caesarCipher('Z.e.B.a.S.t.i.A.n', 9)).toBe('I.n.B.j.B.c.r.A.w');
+});
